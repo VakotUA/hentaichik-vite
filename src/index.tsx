adb8f909221cc2c07022c0e889a0fb0ed6c44b14
VakotUA <90602514+VakotUA@users.noreply.github.com>
2023-06-04 18:14:15 +0300

    Update index.tsx
    fix: add baseurl to avoid loose project name on remote

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="hentaichik-vite">
       <Provider store={store}>
         <Main>
           <Routes>
